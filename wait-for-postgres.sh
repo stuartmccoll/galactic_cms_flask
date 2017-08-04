1389f6db09abf5800e212ce11059753030d6b239
Stuart McColl <contact@stuartmccoll.co.uk>
2017-08-04 20:33:55 +0100

    Travis-CI dependencies

--- wait-for-postgres.sh
+++ wait-for-postgres.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
-set -e
+postgres_host="$1"
+postgres_port="$2"
 
 shift
 cmd="$@"
 
-until psql -h "db-galactic-cms" -U "postgres" -c '\l'; do
+until pg_isalready -h "$postgres_host" -p "$postgres_port" -q; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
 
 >&2 echo "Postgres is up - executing command"
+
+# run the command
 exec $cmd
